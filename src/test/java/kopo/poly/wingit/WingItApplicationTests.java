08e6e1287efad6b6e43eec9e5128f886ddb579ad
bingguxi <kongsuni3685@gmail.com>
2024-05-16 15:43:41 +0900

    WingIt 프로젝트 생성

---
+++ src/test/java/kopo/poly/wingit/WingItApplicationTests.java
@@ -0,0 +1,13 @@
+package kopo.poly.wingit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WingItApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
